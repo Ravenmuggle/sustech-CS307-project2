c9f179e1e92d5b83938e9699bb1149b1a9b96516
puremuggle <12311615@sustech.edu.cn>
2025-05-18 17:48:18 +0800

    initial

--- src/main/java/edu/sustech/cs307/storage/LRUReplacer.java
+++ src/main/java/edu/sustech/cs307/storage/LRUReplacer.java
@@ -14,18 +14,47 @@ public class LRUReplacer {
     }
 
     public int Victim() {
+        if (LRUList.size()>maxSize){
+            Iterator<Integer> reverseIterator=LRUList.descendingIterator();
+            int FirstDeletable=reverseIterator.next();
+            while (pinnedFrames.contains(FirstDeletable){
+                FirstDeletable=reverseIterator.next();
+                //iterating continuing
+            }
+            LRUList.removeLastOccurrence(FirstDeletable);
+            LRUHash.remove(FirstDeletable);
+        }
+        else {
+
+        }
         return -1;
     }
 
     public void Pin(int frameId) {
+        if(pinnedFrames.contains(frameId)){
+            //doing nothing
+        } else if (LRUHash.contains(frameId)) {
+            LRUHash.remove(frameId);
+            LRUList.removeFirstOccurrence(frameId);
+        }
+        else{
+            pinnedFrames.add(frameId);
+        }
     }
 
 
     public void Unpin(int frameId) {
+        if (pinnedFrames.contains(frameId)){
+            LRUHash.add(frameId);
+            LRUList.addFirst(frameId);
+            Victim();
+        }
+
     }
 
 
     public int size() {
+
         return LRUList.size() + pinnedFrames.size();
     }
 }
